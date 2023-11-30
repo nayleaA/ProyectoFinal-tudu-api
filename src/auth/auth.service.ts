7cf7d4bd48e35eda4cb4efb5ecfac9a54821d588
nayleaA <naygdz306@gmail.com>
2023-11-30 13:21:21 -0600

    clase de 30-11-2023

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateUsuarioDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { Model } from 'mongoose';
+import { Usuario } from './entities/auth.entity';
+import { InjectModel } from '@nestjs/mongoose';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  create(CreateUsuarioDto: CreateUsuarioDto) {
-    return 'This action adds a new auth';
+  constructor(
+    @InjectModel(Usuario.name) private usuarioModel: Model<Usuario>,
+  ) {}
+
+  async create(CreateUsuarioDto: CreateUsuarioDto): Promise<Usuario> {
+    try {
+      CreateUsuarioDto.password = bcrypt.hashSync(
+        CreateUsuarioDto.password,
+        10,
+      )
+      const newUsuario = new this.usuarioModel(CreateUsuarioDto);
+      await newUsuario.save();
+      const usuario = newUsuario.toJSON();
+      return usuario;
+    } catch (error) {
+      console.log(error);
+      if (error.code == 11000) {
+        throw new BadRequestException(
+          `${CreateUsuarioDto.email} ya está registrado`,
+        );
+      }
+      throw new InternalServerErrorException('Mi primera chamba');
+    }
   }
 
   findAll() {
